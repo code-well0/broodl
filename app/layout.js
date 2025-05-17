beb1fd5f9c7669187d509c119f92437be9443b1e
code_well <shubralijain@gmail.com>
2025-05-17 14:35:18 +0530

    updates

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
       <Link href={'https://youtu.be/lkjrUW8fI40'} target="_blank" className="">
-        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Built by Smoljames 💛</p>
+        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Built by Shubrali Jain 💛</p>
       </Link>
     </footer>
   )
